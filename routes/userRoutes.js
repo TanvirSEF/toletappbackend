38331d8738a8365b1e968bdbafedff8760d33bc2
Tanvir <tanvirmern@gmail.com>
2025-06-22 15:27:10 +0600

    ss

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,7 @@ const { protect, authorize } = require('../middlewares/authMiddleware');
 const { requestUpgrade, getUpgradeRequests, approveUpgrade } = require('../controllers/userController');
 
 router.post('/upgrade/request', protect, authorize('renter'), requestUpgrade);
-
 router.get('/upgrade/requests', protect, authorize('admin'), getUpgradeRequests);
-
-
 router.put('/upgrade/approve/:id', protect, authorize('admin'), approveUpgrade);
 
 module.exports = router;
