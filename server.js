893ca7e14052da841a35304ec20af516851d7fe3
Tanvir <tanvirmern@gmail.com>
2025-06-22 11:51:00 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('To-Let Backend API Running!');
+});
+
+// Database Connect
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected'))
+  .catch((err) => console.log(err));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
